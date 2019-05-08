53cf37828e3aa80c4ad743a114bef88c8fa23e8d
Garrett Rademacher <rademacher.garrett@gmail.com>
2019-05-08 19:14:18 -0400

    Bug Fixes / PageRank
    Fixed minor bugs in FormatEdges.java. Added the basic structure for the PankRank Hadoop program.

--- PageRank.java
+++ PageRank.java
@@ -13,36 +13,26 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class FormatEdges{
-  public static class EdgeMapper extends Mapper<Object, Text, LongWritable, Text>{
+
+public class PageRank{
+  public static class RankMapper extends Mapper<Object, Text, LongWritable, Text>{
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException{
-      try{
-        context.write(new LongWritable(Long.parseLong(value.toString().split("[\\s\t]+")[0])), new Text(value.toString().split("[\\s\t]+")[1]));
-      }catch(Exception e){
-        System.out.println("Bad String: " + value.toString());
-        e.printStackTrace();
-      }
+
     }
   }
 
-  public static class EdgeReducer extends Reducer<LongWritable, Text, LongWritable, Text>{
+  public static class RankReducer extends Reducer<LongWritable, Text, LongWritable, Text>{
     public void reduce(LongWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException{
-      int total = 0;
-      String list = "";
-      for(Text i : values){
-        total++;
-        list += i.toString() + " ";
-      }
-      context.write(key, new Text(total + ": " + list));
+
     }
   }
 
   public static void main(String[] args) throws Exception{
     Configuration conf = new Configuration();
-		Job job = Job.getInstance(conf, "Format Edges");
-		job.setJarByClass(FormatEdges.class);
-		job.setMapperClass(EdgeMapper.class);
-		job.setReducerClass(EdgeReducer.class);
+		Job job = Job.getInstance(conf, "Calculate PageRank");
+		job.setJarByClass(PageRank.class);
+		job.setMapperClass(RankMapper.class);
+		job.setReducerClass(RankReducer.class);
 		job.setMapOutputKeyClass(LongWritable.class);
 		job.setMapOutputValueClass(Text.class);
 		job.setOutputKeyClass(LongWritable.class);
