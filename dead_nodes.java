83fa888a17ff7d8122d5f21cf09902ae6c8cb873
Garrett Rademacher <rademacher.garrett@gmail.com>
2019-05-07 21:22:06 -0400

    Dead Node Checking
    Added a program to check for dead nodes in the graph, still need to get file names for it though.

---
+++ dead_nodes.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.nio.file.*;
+import java.util.stream.Stream;
+
+public class dead_nodes{
+  //file vars
+  private String input_file = "";
+  private String output_file = "";
+
+  public static void main(String[] args) throws IOException{
+    //create output file writer
+    File output_file = new File(output_file);
+    FileWriter output_writer = new FileWriter(output_file);
+
+    //create the input stream
+    Stream<Stream> stream = File.lines(Path.get(input_file));
+    stream.filter(line -> line.split(" ").length == 1).forEach(line -> printToFile(line));
+
+    //close the files and stream
+    stream.close();
+    output_writer.flush();
+    output_writer.close();
+
+  }
+
+  public static void printToFile(String line, FileWriter fw){
+    //write line to a file
+    try{
+      fw.write(line);
+    }catch(IOException e){
+      e.printStackTrace();
+    }
+  }
+}
